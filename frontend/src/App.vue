a9a391f61b165fb22ccc1134e29b89778af44454
Nikita Developer <nik0609tak@gmail.com>
2025-06-09 20:54:02 +0200

    set up

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: 'Hello, Vue!'
+    };
+  },
+  mounted() {
+    console.log(this.message);
+  }
+};
+</script>
+
+<template>
+  <h1>Time Tile</h1>
+</template>
+
+<style scoped>
+</style>
