692510ef11c08a5da3ec8cc8dffc4e6446ff5908
Kliushyn Yehor <egorka231231@gmail.com>
2025-07-15 12:25:51 +0200

    GradesCourseCard renders actual icon

--- frontend/src/components/student/GradesCourseCard.vue
+++ frontend/src/components/student/GradesCourseCard.vue
@@ -32,7 +32,7 @@ const props = defineProps<{
         </div>
         <img
           class="h-18 w-18 rounded-md"
-          :src="'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'"
+          :src="courseInfo.course.iconUrl || 'https://via.placeholder.com/72'"
           alt=""
         />
       </div>
